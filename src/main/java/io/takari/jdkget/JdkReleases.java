c32023f7f2a9cb1deef4ca14906533f7d1c78862
Donald Morton <Donald.Morton@walmart.com>
2017-01-25 14:34:32 -0600

    removing imports that were only added for debugging

--- src/main/java/io/takari/jdkget/JdkReleases.java
+++ src/main/java/io/takari/jdkget/JdkReleases.java
@@ -10,8 +10,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import javax.net.ssl.HttpsURLConnection;
 import io.takari.jdkget.JdkGetter.JdkVersion;
 
